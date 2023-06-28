f266a7e8bb218f3f2ec0fd44d599ef23a0b96786
madhu7544 <sahukari.kumar.24.1@mountblue.tech>
2023-06-28 15:45:15 +0530

    modification on posts_controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+    # before_action :find_post, only: [:show, :update, :edit, :destroy] 
 
     def index
         @posts = Post.all.order("created_at DESC")
